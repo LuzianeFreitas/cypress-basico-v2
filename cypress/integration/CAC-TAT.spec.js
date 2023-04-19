52e4d35bb977535c3b409274a20b547b1a480ba7
LuzianeFreitas07 <luziane.freitas@convenia.com.br>
2023-04-19 10:26:08 -0300

    add: tests

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,9 +1,49 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-  it('verifica o título da aplicação', function() {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  })
+
+  it('verifica o título da aplicação', function() {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', function() {
+    const longText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+    
+    cy.get('#firstName').type('Luziane')
+    cy.get('#lastName').type('Freitas')
+    cy.get('#email').type('luziane@exemplo.com')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    cy.get('button[type="submit"]').click()
+    cy.get('.success').should('be.visible')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', function() {
+    const longText = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+    
+    cy.get('#firstName').type('Luziane')
+    cy.get('#lastName').type('Freitas')
+    cy.get('#email').type('luziane@exemplo.com')
+    cy.get('#open-text-area').type(longText, { delay: 0 })
+    cy.get('button[type="submit"]').click()
+    cy.get('.success').should('be.visible')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
+    cy.get('#firstName').type('Luziane')
+    cy.get('#lastName').type('Freitas')
+    cy.get('#email').type('luziane#exemplo.com')
+    cy.get('#open-text-area').type('teste descrição')
+    cy.get('button[type="submit"]').click()
+    cy.get('.error').should('be.visible')
+  })
+
+  it('campo de telefone continua vazio quando preenchido com valor não-numérico', function() {
+    cy.get('#phone')
+      .type('ajhdhfgsvdtsda')
+      .should('have.value', '')
   })
 })
 
